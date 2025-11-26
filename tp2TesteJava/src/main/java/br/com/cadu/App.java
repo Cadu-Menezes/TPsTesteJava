27930f046b26f9e400aa4ee979d04f9f590acb1b
Cadu-Menezes <cadumenezes051@gmail.com>
2025-11-26 14:46:05 -0300

    first

---
+++ tp2TesteJava/src/main/java/br/com/cadu/App.java
@@ -0,0 +1,15 @@
+package br.com.cadu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        // Compilar o projeto: mvn compile
+        // Rodar o projeto: java -cp target/classes br.com.cadu.App
+        System.out.println( "Hello World!" );
+    }
+}
